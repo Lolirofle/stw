82215038ef64f8d9c4900e8453d682acb5b739b1
Lolirofle <lolipopple@hotmail.com>
2017-07-01 02:50:41 +0200

    Seems to work now

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,19 @@ use nalgebra::{Vector2,Point2,Isometry2,Translation2};
 
 struct Pong;
 
-struct Object{
-	pub position: Vector2<f32>,
-	pub velocity: Vector2<f32>,
+struct Position(pub Vector2<f32>);
+impl Component for Position {
+	type Storage = VecStorage<Position>;
 }
 
-impl Component for Object {
-	type Storage = VecStorage<Object>;
+struct Velocity(pub Vector2<f32>);
+impl Component for Velocity {
+	type Storage = VecStorage<Velocity>;
 }
 
 struct Ball {
 	pub size: f32,
 }
-
 impl Component for Ball {
 	type Storage = VecStorage<Ball>;
 }
@@ -38,13 +38,11 @@ struct Plank {
 	pub dimensions: Vector2<f32>,
 	pub side: Side,
 }
-
 impl Component for Plank {
 	type Storage = VecStorage<Plank>;
 }
 
 struct PongSystem;
-
 unsafe impl Sync for PongSystem {}
 
 struct Score {
@@ -59,14 +57,17 @@ impl System<()> for PongSystem {
 		use amethyst::ecs::resources::{Camera, InputHandler, Projection, Time};
 
 		// Get all needed component storages and resources
-		let (mut balls, planks, mut objs, locals, camera, time, input, mut score) = arg.fetch(|w| (
+		let (mut balls, planks, positions, velocities, (locals, camera, time, input), mut score) = arg.fetch(|w| (
 			w.write::<Ball>(),
 			w.write::<Plank>(),
-			w.write::<Object>(),
-			w.write::<LocalTransform>(),
-			w.read_resource::<Camera>(),
-			w.read_resource::<Time>(),
-			w.read_resource::<InputHandler>(),
+			w.write::<Position>(),
+			w.write::<Velocity>(),
+			(
+				w.write::<LocalTransform>(),
+				w.read_resource::<Camera>(),
+				w.read_resource::<Time>(),
+				w.read_resource::<InputHandler>(),
+			),
 			w.write_resource::<Score>()
 		));
 
@@ -86,77 +87,74 @@ impl System<()> for PongSystem {
 
 		let delta_time = time.delta_time.subsec_nanos() as f32 / 1.0e9;
 
-		let mut locals = locals.pass();
-		let mut objs   = objs.pass();
+		let mut locals     = locals.pass();
+		let mut positions  = positions.pass();
+		let mut velocities = velocities.pass();
 
 		// Process all planks
-		for (plank, obj, local) in (&mut planks.pass(), &mut objs, &mut locals).join() {
+		for (ref mut plank, &mut Position(ref mut position), &mut Velocity(ref mut velocity), ref mut local) in (&mut planks.pass(), &mut positions, &mut velocities, &mut locals).join() {
 			match plank.side {
 				// If it is a left plank
 				Side::Left => {
 					// Store left plank position for later use in ball processing
-					left_position = obj.position[1];
+					left_position = position[1];
 					// Store left plank dimensions for later use in ball processing
 					left_dimensions = plank.dimensions;
 					// If `W` is pressed and plank is in screen boundaries then move up
 					if input.key_down(VirtualKeyCode::W) {
-						if obj.position[1] + plank.dimensions[1] / 2. < 1. {
-							obj.position += obj.velocity * delta_time;
+						if position[1] + plank.dimensions[1] / 2. < 1. {
+							*velocity = Vector2::new(0.0,1.0);
 						}
 					}
 					// If `S` is pressed and plank is in screen boundaries then move down
 					if input.key_down(VirtualKeyCode::S) {
-						if obj.position[1] - plank.dimensions[1] / 2. > -1. {
-							obj.position -= obj.velocity * delta_time;
+						if position[1] - plank.dimensions[1] / 2. > -1. {
+							*velocity = Vector2::new(0.0,-1.0);
 						}
 					}
 				}
 				// If it is a right plank
 				Side::Right => {
 					// Store right plank position for later use in ball processing
-					right_position = obj.position[1];
+					right_position = position[1];
 					// Store right plank dimensions for later use in ball processing
 					right_dimensions = plank.dimensions;
 					// If `Up` is pressed and plank is in screen boundaries then move down
 					if input.key_down(VirtualKeyCode::Up) {
-						if obj.position[1] + plank.dimensions[1] / 2. < top_bound {
-							obj.position += obj.velocity * delta_time;
+						if position[1] + plank.dimensions[1] / 2. < top_bound {
+							*velocity = Vector2::new(0.0,1.0);
 						}
 					}
 					// If `Down` is pressed and plank is in screen boundaries then move down
 					if input.key_down(VirtualKeyCode::Down) {
-						if obj.position[1] - plank.dimensions[1] / 2. > bottom_bound {
-							obj.position -= obj.velocity * delta_time;
+						if position[1] - plank.dimensions[1] / 2. > bottom_bound {
+							*velocity = Vector2::new(0.0,-1.0);
 						}
 					}
 				}
 			};
 			// Set translation of renderable corresponding to this plank
-			local.translation[0] = obj.position[0];
-			local.translation[1] = obj.position[1];
+			local.translation[0] = position[0];
+			local.translation[1] = position[1];
 			// Set scale for renderable corresponding to this plank
 			local.scale = [plank.dimensions[0], plank.dimensions[1], 1.0];
 		}
 
 		// Process the ball
-		for (ball, obj, local) in (&mut balls, &mut objs, &mut locals).join() {
-			// Move the ball
-			obj.position[1] += obj.velocity[1] * delta_time;
-			obj.position[0] += obj.velocity[0] * delta_time;
-
+		for (ref mut ball, &mut Position(ref mut position), &mut Velocity(ref mut velocity), ref mut local) in (&mut balls, &mut positions, &mut velocities, &mut locals).join() {
 			// Check if the ball has collided with the right plank
-			if obj.position[0] + ball.size / 2. > right_bound - left_dimensions[0] &&
-			   obj.position[0] + ball.size / 2. < right_bound {
-				if obj.position[1] - ball.size / 2. < right_position + right_dimensions[1] / 2. &&
-				   obj.position[1] + ball.size / 2. > right_position - right_dimensions[1] / 2. {
-					obj.position[0] = right_bound - right_dimensions[0] - ball.size / 2.;
-					obj.velocity[0] = -obj.velocity[0];
+			if position[0] + ball.size / 2. > right_bound - left_dimensions[0] &&
+			   position[0] + ball.size / 2. < right_bound {
+				if position[1] - ball.size / 2. < right_position + right_dimensions[1] / 2. &&
+				   position[1] + ball.size / 2. > right_position - right_dimensions[1] / 2. {
+					position[0] = right_bound - right_dimensions[0] - ball.size / 2.;
+					velocity[0] = -velocity[0];
 				}
 			}
 
 			// Check if the ball is to the left of the right boundary, if it is not reset it's position and score the left player
-			if obj.position[0] - ball.size / 2. > right_bound {
-				obj.position[0] = 0.;
+			if position[0] - ball.size / 2. > right_bound {
+				position[0] = 0.;
 				score.score_left += 1;
 				println!("Left player score: {0}, Right player score {1}",
 						 score.score_left,
@@ -164,18 +162,18 @@ impl System<()> for PongSystem {
 			}
 
 			// Check if the ball has collided with the left plank
-			if obj.position[0] - ball.size / 2. < left_bound + left_dimensions[0] &&
-			   obj.position[0] + ball.size / 2. > left_bound {
-				if obj.position[1] - ball.size / 2. < left_position + left_dimensions[1] / 2. &&
-				   obj.position[1] + ball.size / 2. > left_position - left_dimensions[1] / 2. {
-					obj.position[0] = left_bound + left_dimensions[0] + ball.size / 2.;
-					obj.velocity[0] = -obj.velocity[0];
+			if position[0] - ball.size / 2. < left_bound + left_dimensions[0] &&
+			   position[0] + ball.size / 2. > left_bound {
+				if position[1] - ball.size / 2. < left_position + left_dimensions[1] / 2. &&
+				   position[1] + ball.size / 2. > left_position - left_dimensions[1] / 2. {
+					position[0] = left_bound + left_dimensions[0] + ball.size / 2.;
+					velocity[0] = -velocity[0];
 				}
 			}
 
 			// Check if the ball is to the right of the left boundary, if it is not reset it's position and score the right player
-			if obj.position[0] + ball.size / 2. < left_bound {
-				obj.position[0] = 0.;
+			if position[0] + ball.size / 2. < left_bound {
+				position[0] = 0.;
 				score.score_right += 1;
 				println!("Left player score: {0}, Right player score {1}",
 						 score.score_left,
@@ -183,22 +181,28 @@ impl System<()> for PongSystem {
 			}
 
 			// Check if the ball is below the top boundary, if it is not deflect it
-			if obj.position[1] + ball.size / 2. > top_bound {
-				obj.position[1] = top_bound - ball.size / 2.;
-				obj.velocity[1] = -obj.velocity[1];
+			if position[1] + ball.size / 2. > top_bound {
+				position[1] = top_bound - ball.size / 2.;
+				velocity[1] = -velocity[1];
 			}
 
 			// Check if the ball is above the bottom boundary, if it is not deflect it
-			if obj.position[1] - ball.size / 2. < bottom_bound {
-				obj.position[1] = bottom_bound + ball.size / 2.;
-				obj.velocity[1] = -obj.velocity[1];
+			if position[1] - ball.size / 2. < bottom_bound {
+				position[1] = bottom_bound + ball.size / 2.;
+				velocity[1] = -velocity[1];
 			}
 
 			// Update the renderable corresponding to this ball
-			local.translation[0] = obj.position[0];
-			local.translation[1] = obj.position[1];
+			local.translation[0] = position[0];
+			local.translation[1] = position[1];
 			local.scale[0] = ball.size;
 			local.scale[1] = ball.size;
+		}
+
+		//Process position from velocity
+		for (&mut Position(ref mut position), &mut Velocity(ref mut velocity)) in (&mut positions, &mut velocities).join() {
+			position[0] += velocity[0] * delta_time;
+			position[1] += velocity[1] * delta_time;
 		}
 	}
 }
@@ -262,10 +266,8 @@ impl State for Pong {
 			world.create_now()
 				.with(square.clone())
 				.with(ball)
-				.with(Object{
-					position: Vector2::new(0.0,0.0),
-					velocity: Vector2::new(1.0,1.0),
-				})
+				.with(Position(Vector2::new(0.0,0.0)))
+				.with(Velocity(Vector2::new(1.0,1.0)))
 				.with(LocalTransform::default())
 				.with(Transform::default())
 				.build();
@@ -277,14 +279,12 @@ impl State for Pong {
 				dimensions: Vector2::new(0.1,0.3),
 				side      : Side::Left,
 			};
-			let obj = Object{
-				position: Vector2::new(-1.0 + plank.dimensions[0] / 2.0,0.0),
-				velocity: Vector2::new(0.0,1.0),
-			};
+			let pos = Position(Vector2::new(-1.0 + plank.dimensions[0] / 2.0,0.0));
 			world.create_now()
 				.with(square.clone())
 				.with(plank)
-				.with(obj)
+				.with(pos)
+				.with(Velocity(Vector2::new(0.0,0.0)))
 				.with(LocalTransform::default())
 				.with(Transform::default())
 				.build();
@@ -296,14 +296,12 @@ impl State for Pong {
 				dimensions: Vector2::new(0.1,0.3),
 				side      : Side::Right,
 			};
-			let obj = Object{
-				position: Vector2::new(1.0 + plank.dimensions[0] / 2.0,0.0),
-				velocity: Vector2::new(0.0,1.0),
-			};
+			let pos = Position(Vector2::new(1.0 + plank.dimensions[0] / 2.0,0.0));
 			world.create_now()
 				.with(square.clone())
 				.with(plank)
-				.with(obj)
+				.with(pos)
+				.with(Velocity(Vector2::new(0.0,0.0)))
 				.with(LocalTransform::default())
 				.with(Transform::default())
 				.build();
@@ -333,7 +331,8 @@ fn main() {
 	let mut game = Application::build(Pong, cfg)
 		.register::<Ball>()
 		.register::<Plank>()
-		.register::<Object>()
+		.register::<Position>()
+		.register::<Velocity>()
 		.with::<PongSystem>(PongSystem, "pong_system", 1)
 		.done();
 	game.run();
