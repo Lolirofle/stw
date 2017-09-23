648cd10afc12d7dc105b639e59b7b34afc88091a
Jesper Fridefors <jesper.fridefors@gmail.com>
2017-09-23 23:46:34 +0200

    updated to developer branch because standard 0.5.1 crashes on launch on certain systems

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#![feature(associated_consts)]
 extern crate alga;
 extern crate amethyst;
 extern crate amethyst_input;
@@ -15,11 +14,21 @@ mod systems;
 
 use amethyst::Application;
 use amethyst_renderer::Config as DisplayConfig;
-use amethyst_renderer::{Stage, Pipeline, pass};
-use amethyst_renderer::vertex::PosNormTex;
-use amethyst::ecs::transform::TransformSystem;
+use amethyst_renderer::prelude::*;
+use amethyst::ecs::transform::{Transform, TransformSystem};
+use amethyst::ecs::rendering::{MeshComponent, MaterialComponent, RenderBundle};
+
+type DrawFlat = pass::DrawFlat<PosNormTex, MeshComponent, MaterialComponent, Transform>;
 
 fn main(){
+	let cfg = {
+			let mut cfg = DisplayConfig::default();
+			cfg.title          = "STW3".to_owned();
+			cfg.dimensions     = Some((640,480));
+			cfg.min_dimensions = Some((640,480));
+			cfg.multisampling  = 0;
+			cfg
+	};
 	Application::build(
 		states::Ingame
 	)
@@ -28,24 +37,17 @@ fn main(){
 	.register::<components::Player>()
 	.register::<components::Position>()
 	.register::<components::CollisionCache>()
-	.with::<systems::ingame::PlayerInput>(systems::ingame::PlayerInput   ,"input_system"    ,&[])
-	.with::<systems::ingame::Physics>    (systems::ingame::Physics::new(),"physics_system"  ,&[])
-	.with::<systems::ingame::Render>     (systems::ingame::Render        ,"render_system"   ,&[])
-	.with::<TransformSystem>             (TransformSystem::new()         ,"transform_system",&["input_system","physics_system","render_system"])
-	.with_renderer(
-		Pipeline::build().with_stage(
-			Stage::with_backbuffer()
-				.clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
-				.with_model_pass(pass::DrawFlat::<PosNormTex>::new())
-		),
-		Some({
-			let mut cfg = DisplayConfig::default();
-			cfg.title          = "STW3".to_owned();
-			cfg.dimensions     = Some((640,480));
-			cfg.min_dimensions = Some((640,480));
-			cfg.multisampling  = 0;
-			cfg
-		})
+	.with::<systems::ingame::PlayerInput>(systems::ingame::PlayerInput, "input_system", &[])
+	.with::<systems::ingame::Physics>(systems::ingame::Physics::new(), "physics_system", &[])
+	.with::<systems::ingame::Render>(systems::ingame::Render, "render_system", &[])
+	.with::<TransformSystem>(TransformSystem::new(), "transform_system", &["physics_system"])
+	.with_bundle(
+		RenderBundle::new(
+			Pipeline::build().with_stage(
+				Stage::with_backbuffer()
+					.clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+					.with_pass(DrawFlat::new())
+		)).with_config(cfg)
 	)
 	.unwrap().build().unwrap().run();
 }
